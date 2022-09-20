8e2f62ec6f0950eefab36cba3b141b2fdab3374d
Ankit Ghosh <ankitghosh877@gmail.com>
2022-09-20 23:06:39 +0530

    greater or lesser the line

--- src/com/codinclub/line.java
+++ src/com/codinclub/line.java
@@ -11,10 +11,13 @@ public class line {
     float length() {
         return (float) Math.sqrt(Math.pow((p3.x - p4.x), 2) + Math.pow((p3.y - p4.y), 2));
     }
-        static void checkEqual(float length1,float length2){
-            if(length1 == length2 ){
-                System.out.println("Both the lines are equal");
-            }else
-                System.out.println("Both the lines are not equal");
+    static void compareLine(float length1, float length2) {
+        if (length1 >= length2) {
+            System.out.println("line one is greater than line two.");
+        } else if (length1 <= length2) {
+            System.out.println("line two is grater than line one.");
+        } else {
+            System.out.println("both line is equal.");
         }
+    }
 }
